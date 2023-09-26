10d41762ad6d3cf8e645722bbe51710b8d6112c4
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-26 21:54:21 +0300

    76 feature: categories app created

---
+++ src/apps/categories/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
