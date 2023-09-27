02acd2d332fffd0aa4fd91d5304965d2d24490e3
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-27 11:43:35 +0300

    76 feature: add idea in ignore

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.categories.apps.CategoriesConfig"
+    "src.apps.categories"
 ]
 
 MIDDLEWARE = [
