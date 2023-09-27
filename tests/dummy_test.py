2378c156f636609785621f31f4534248f979cd01
gluhovSemen <rusglu03@mail.ru>
2023-09-27 14:00:54 +0200

    feat: added test for django import

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,4 +1,11 @@
 # TODO this file will be deleted when actual tests will be ready.
 
+from manage import main
+
+
 def test_dummy_test():
     assert 1 == 1
+
+
+def test_dependency_test():
+    main()
