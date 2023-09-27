4be109a2b12d538821abcde32c6a6fddbb096927
KateMzz <84163327+KateMzz@users.noreply.github.com>
2023-09-27 17:33:35 +0300

    Update check.py

--- formatters_checker/check.py
+++ formatters_checker/check.py
@@ -1,8 +1,11 @@
-word="We recommend the use of the @stable tag, but per version tags also
-exist if you prefer that. Note that the action’s version you select 
-is independent of the version of Black the action will use."
+# word="We recommend the use of the @stable tag, but per version tags also
+# exist if you prefer that. Note that the action’s version you select 
+# is independent of the version of Black the action will use."
 
-new= "The version of Black the action will use can be configured via version. 
-This can be any valid version specifier or just the version number if you want an exact version.
-The action defaults to the latest release available on PyPI. 
-Only versions available from PyPI are supported, so no commit SHAs or branch names."
+# new= "The version of Black the action will use can be configured via version. 
+# This can be any valid version specifier or just the version number if you want an exact version.
+# The action defaults to the latest release available on PyPI. 
+# Only versions available from PyPI are supported, so no commit SHAs or branch names."
+
+
+hello = "World"
