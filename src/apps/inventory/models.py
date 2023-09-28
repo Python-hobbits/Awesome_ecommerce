d43b602adf354b699e1da81900424738b19de9dc
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-28 10:24:38 +0300

    76 feature: product model added, app changed

--- src/apps/inventory/models.py
+++ src/apps/inventory/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
