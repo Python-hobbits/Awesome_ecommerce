93e81f5e8f2635ca241e1158c0b281e531c2f4f1
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-28 10:17:28 +0300

    76 feature: product model added, app changed

--- src/apps/categories/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
