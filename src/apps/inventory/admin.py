a74fabf92c7a039b4069c1560930e5eb894f52ac
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-28 12:22:43 +0300

    76 feature: fixed after review

--- src/apps/inventory/admin.py
+++ src/apps/inventory/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Category
-from .models import Product
-
+from src.apps.inventory.models import Category, Product
 
 admin.site.register(Category)
 admin.site.register(Product)
--- src/apps/inventory/models.py
+++ src/apps/inventory/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
