940f6fa10d25b510a835088aa43dd3693dcd5021
gluhovSemen <44681620+gluhovSemen@users.noreply.github.com>
2023-09-29 16:37:04 +0200

    feat product list for seller done (#12)
    * feat product list for seller done
    
    * feat codereview fixes
    
    * feat  migrations added
    
    * fix migrations and user reference fixed
    
    * fix: return none returns)

--- src/apps/inventory/models.py
+++ src/apps/inventory/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -14,6 +15,13 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="products",
+        limit_choices_to={"user_type": "Seller"},
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
