77c31d9e06a0c906964c4ad7cb67b3c767f3f1af
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2023-09-30 23:47:10 +0300

    92 feature: added basket app + context processor

---
+++ src/apps/basket/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from src.apps.basket.models import Basket
+
+admin.site.register(Basket)
---
+++ src/apps/basket/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from src.apps.inventory.models import Product
+from src.apps.user.models import User
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'Basket for {self.user.username}: {self.quantity} x {self.product.name}'
