b4de3096a40524b2df5612873e72624174838f07
kleikoks <64419160+kleikoks@users.noreply.github.com>
2023-10-02 13:07:53 +0300

    update environment setup (#15)
    * update environment setup
    
    * linter checks passed

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,17 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
-from pathlib import Path
-
 import environ
 
-env = environ.Env()
-env_path = os.path.join(os.path.dirname(__file__), "..", "..", ".env")
-environ.Env.read_env(env_file=env_path)
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(env_file=BASE_DIR.parent / ".env")
 
 
 # Quick-start development settings - unsuitable for production
