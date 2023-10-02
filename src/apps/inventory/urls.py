8230bbe89d5801952a05b92f1f3b1c70518fb801
gluhovSemen <44681620+gluhovSemen@users.noreply.github.com>
2023-10-02 15:24:25 +0200

    feat: profile and profile edit added (#16)
    * feat: profile and profile edit added
    
    * PR conflict resolving
    
    * fix: comments deleted
    
    * fix all imports fixed
    
    * Revert "fix all imports fixed"
    
    This reverts commit f4cd6c46fbbb77af061d42b876ebdae2e32f6b3e.
    
    * fix imports fix without migrations
    
    * fix: order of imports
    
    * fix: indentation fixed
    
    * fix: user reference and changes in settings fixed
    
    * feat: tests for user profile done
    
    * fix action fixed1
    
    * fix profile is now class

--- src/apps/inventory/urls.py
+++ src/apps/inventory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from src.apps.inventory.views import ProductDetailView, ProductBySellerListView
+from src.apps.inventory.views import ProductBySellerListView, ProductDetailView
 
 urlpatterns = [
     path("<pk>/", ProductDetailView.as_view()),
--- src/apps/orders/models.py
+++ src/apps/orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from src.apps.inventory.models import Product
 
 
