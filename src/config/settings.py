fb3a783d8d699ebd68a495153321bc0ba59c26fc
kleikoks <kleikoks.py@gmail.com>
2023-10-02 13:02:41 +0300

    linter checks passed

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
-from pathlib import Path
-
 import environ
+
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
