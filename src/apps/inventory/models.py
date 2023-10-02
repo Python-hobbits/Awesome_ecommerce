31bbc334cf0b8ea15f7fc2b037cf4612e83a1e6b
gluhovSemen <rusglu03@mail.ru>
2023-10-02 14:40:30 +0200

    feat Product and category paths changed

--- src/apps/inventory/models.py
+++ src/apps/inventory/models.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    slug = models.SlugField(default=str(name))
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        # Define the URL for a category
+        return f"/category/{self.slug}/"  # Adjust the URL structure as needed
 
 
 class Product(models.Model):
@@ -22,6 +32,14 @@ class Product(models.Model):
         limit_choices_to={"user_type": "Seller"},
     )
     created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(default=str(name))
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return f"/product/{self.category.slug}/{self.slug}/"  # Adjust the URL structure as needed
--- src/apps/inventory/views.py
+++ src/apps/inventory/views.py
@@ -1,10 +1,25 @@
 from django.views.generic import DetailView, ListView
 
-from src.apps.inventory.models import Product
+from src.apps.inventory.models import Product, Category
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
+    slug_url_kwarg = "product_slug"
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = "category_detail.html"
+    context_object_name = "category"
+    slug_url_kwarg = "category_slug"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.filter(category=self.object)
+        return context
 
 
 class ProductBySellerListView(ListView):
@@ -15,3 +30,9 @@ class ProductBySellerListView(ListView):
         if self.request.user.is_authenticated:
             return Product.objects.filter(seller=self.request.user)
         return Product.objects.none()
+
+
+class ProductListView(ListView):
+    model = Product
+    paginate_by = 10
+    template_name = "product_list.html"
