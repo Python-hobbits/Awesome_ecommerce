eda75890b45c948e6a7e1243387fafaad1903480
gluhovSemen <44681620+gluhovSemen@users.noreply.github.com>
2023-10-09 10:06:53 +0200

    Feature/123/create support pages based on e com template (#23)
    * feat: about us added
    
    * feat delivery added
    
    * Feat: Terms and Conditions
    
    * feat contact information added
    
    * fix: html structure
    
    * fix: title added
    
    * fix template title block deleted
    
    * Revert "fix template title block deleted"
    
    This reverts commit cf573888652a23f2daa1d01f8fb7ca7b657a94a6.
    
    * fix: title added

--- src/apps/content/views.py
+++ src/apps/content/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "content/index.html"
+
+
+class AboutUsView(TemplateView):
+    template_name = "support/about_us.html"
+
+
+class DeliveryView(TemplateView):
+    template_name = "support/delivery.html"
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = "support/terms_and_conditions.html"
