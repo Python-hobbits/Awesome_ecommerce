dea93473c626688e92ab07595b8231d10abf9e8c
Viacheslav Volodin <volodin28@gmail.com>
2023-10-13 10:12:30 +0300

    add Mixin to CheckoutView, use DetailView for ThankYou

--- src/apps/orders/views.py
+++ src/apps/orders/views.py
@@ -1,95 +1,88 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
+from django.views.generic import DetailView
 
 from src.apps.basket.basket import Basket
 from .forms import DeliveryOptionForm
 from .models import Order, OrderProduct
 
 
-class CheckoutView(View):
+class CheckoutView(LoginRequiredMixin, View):
     template_name = "orders/checkout.html"
 
     def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            basket = Basket(request)
-            total_price = basket.get_total_price()
-            delivery_option_form = DeliveryOptionForm()
-            return render(
-                request,
-                self.template_name,
-                {
-                    "basket": basket,
-                    "total_price": total_price,
-                    "delivery_option_form": delivery_option_form,
-                },
-            )
+        basket = Basket(request)
+        total_price = basket.get_total_price()
+        delivery_option_form = DeliveryOptionForm()
 
-        else:
-            return HttpResponseRedirect(reverse("account_login"))
+        return render(
+            request,
+            self.template_name,
+            {
+                "basket": basket,
+                "total_price": total_price,
+                "delivery_option_form": delivery_option_form,
+            },
+        )
 
     def post(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return HttpResponseRedirect(reverse("account_login"))
-        else:
-            basket = Basket(request)
-            total_price = basket.get_total_price()
-            delivery_option_form = DeliveryOptionForm(request.POST)
+        basket = Basket(request)
+        total_price = basket.get_total_price()
+        delivery_option_form = DeliveryOptionForm(request.POST)
 
-            if delivery_option_form.is_valid():
-                order = Order.objects.create(
-                    user_id=request.user,
-                    user_name=request.user.first_name,
-                    user_email=request.user.email,
-                )
-
-                for item in basket:
-                    product = item["product"]
-                    quantity = item["quantity"]
-                    price = item["price"]
-                    OrderProduct.objects.create(
-                        order=order, product_id=product, quantity=quantity, product_price=price
-                    )
-
-                delivery_method = delivery_option_form.save(commit=False)
-                delivery_method.order = order
-                delivery_method.save()
-
-                basket.clear()
-
-                return HttpResponseRedirect(reverse("thank_you", kwargs={"order_id": order.id}))
-
-            return render(
-                request,
-                self.template_name,
-                {
-                    "basket": basket,
-                    "total_price": total_price,
-                    "delivery_option_form": delivery_option_form,
-                },
+        if delivery_option_form.is_valid():
+            order = Order.objects.create(
+                user_id=request.user,
+                user_name=request.user.first_name,
+                user_email=request.user.email,
             )
 
+            for item in basket:
+                product = item["product"]
+                quantity = item["quantity"]
+                price = item["price"]
+                OrderProduct.objects.create(
+                    order=order, product_id=product, quantity=quantity, product_price=price
+                )
 
-class ThankYouView(UserPassesTestMixin, View):
+            delivery_method = delivery_option_form.save(commit=False)
+            delivery_method.order = order
+            delivery_method.save()
+
+            basket.clear()
+
+            return HttpResponseRedirect(reverse("thank_you", kwargs={"order_id": order.id}))
+
+        return render(
+            request,
+            self.template_name,
+            {
+                "basket": basket,
+                "total_price": total_price,
+                "delivery_option_form": delivery_option_form,
+            },
+        )
+
+
+class ThankYouView(UserPassesTestMixin, DetailView):
+    model = Order
     template_name = "orders/thank_you.html"
+    context_object_name = "order"
+    pk_url_kwarg = "order_id"
 
     def test_func(self):
-        order = self.get_order()
+        order = self.get_object()
         return self.request.user == order.user_id
 
-    def get_order(self):
-        order_id = self.kwargs["order_id"]
-        return get_object_or_404(Order, id=order_id)
-
     def get(self, request, *args, **kwargs):
-        total_price = 0
-
-        order = self.get_order()
-
-        for order_product in order.orderproduct_set.all():
-            total_price += order_product.get_total_price()
+        order = self.get_object()
+        print(order.id)
+        total_price = sum(
+            order_product.get_total_price() for order_product in order.orderproduct_set.all()
+        )
 
         context = {"order": order, "total_price": total_price}
         return render(request, self.template_name, context)
