91f4fb849e80aad332d4c479f0d1cf82f2048733
Viacheslav Volodin <volodin28@gmail.com>
2023-10-19 13:55:09 +0300

    format_fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [BASE_DIR / "static/bootstrap", BASE_DIR / "static/pdfs"]
