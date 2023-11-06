845277f9d0e78d9cb3ceae4749b77e141ed3e3cb
Viacheslav Volodin <volodin28@gmail.com>
2023-11-06 11:51:17 +0200

    change user page and user edit page; remove profile page; update header

--- src/apps/user/urls.py
+++ src/apps/user/urls.py
@@ -3,14 +3,10 @@ from django.urls import include, path
 from src.apps.user.views import (
     UserUpdateView,
     UserDetailView,
-    ProfileUpdateView,
-    UserProfileDetailView,
 )
 
 urlpatterns = [
     path("", include("allauth.urls")),
-    path("user/<slug:uuid>", UserDetailView.as_view(), name="user_detail"),
-    path("edit/<slug:uuid>", UserUpdateView.as_view(), name="user_edit"),
-    path("profile/edit/", ProfileUpdateView.as_view(), name="profile_edit"),
-    path("profile/", UserProfileDetailView.as_view(), name="profile_detail"),
+    path("<slug:uuid>", UserDetailView.as_view(), name="user_detail"),
+    path("<slug:uuid>/edit", UserUpdateView.as_view(), name="user_edit"),
 ]
--- src/apps/user/views.py
+++ src/apps/user/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse
 from django.views.generic import UpdateView, DetailView
 
-from src.apps.user.forms import CustomUserChangeForm, ProfileForm
-from src.apps.user.models import UserProfile, UserAddress
+from src.apps.user.forms import UserForm, UserAddressForm, UserProfileForm
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
@@ -16,50 +15,45 @@ class UserDetailView(LoginRequiredMixin, DetailView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = get_user_model()
-    form_class = CustomUserChangeForm
     template_name = "user/user_edit.html"
+    form_class = UserForm
+    address_form_class = UserAddressForm
+    profile_form_class = UserProfileForm
     slug_field = "uuid"
     slug_url_kwarg = "uuid"
 
     def get_success_url(self):
         return reverse("user_detail", args=[self.object.uuid])
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if "form" not in context:
+            context["form"] = self.form_class(instance=self.object)
+        if "address_form" not in context:
+            context["address_form"] = self.address_form_class(
+                instance=self.object.user_profile.address
+            )
+        if "profile_form" not in context:
+            context["profile_form"] = self.profile_form_class(instance=self.object.user_profile)
+        return context
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
-    model = UserProfile
-    form_class = ProfileForm
-    template_name = "profile/profile_edit.html"
-    success_url = reverse_lazy("profile_detail")
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.form_class(request.POST, instance=self.object)
+        address_form = self.address_form_class(
+            request.POST, instance=self.object.user_profile.address
+        )
+        profile_form = self.profile_form_class(
+            request.POST, request.FILES, instance=self.object.user_profile
+        )
 
-    def get(self, request, *args, **kwargs):
-        profile = self.get_object()
-        if not profile.address:
-            address = UserAddress()
-            address.save()
-            profile.address = address
-            profile.save()
-        return super().get(request, *args, **kwargs)
+        if form.is_valid() and address_form.is_valid() and profile_form.is_valid():
+            form.save()
+            address_instance = address_form.save(commit=False)
+            address_instance.save()
+            profile_instance = profile_form.save(commit=False)
+            profile_instance.address = address_instance
+            profile_instance.save()
+            return self.form_valid(form)
 
-    def get_object(self, queryset=None):
-        profile = self.request.user.user_profile
-        return profile
-
-    def form_valid(self, form):
-        profile = form.save(commit=False)
-        profile_form = ProfileForm(self.request.POST, instance=profile.address)
-
-        if profile_form.is_valid():
-            address = profile_form.save()
-            profile.address = address
-
-        profile.save()
-        return super().form_valid(form)
-
-
-class UserProfileDetailView(LoginRequiredMixin, DetailView):
-    model = UserProfile
-    template_name = "profile/profile_detail.html"
-
-    def get_object(self, queryset=None):
-        profile = self.request.user.user_profile
-        return profile
+        return self.form_invalid(form)
