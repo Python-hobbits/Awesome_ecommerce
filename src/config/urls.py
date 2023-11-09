6cefb780e4247ffbe1f19ebebcc6784743981b51
Viacheslav Volodin <volodin28@gmail.com>
2023-11-09 10:13:27 +0200

    move "__debug__/" in urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     path("inventory/", include("src.apps.inventory.urls")),
     path("checkout/", include("src.apps.orders.urls")),
     path("thank_you/<int:order_id>/", ThankYouView.as_view(), name="thank_you"),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path("__debug__/", include("debug_toolbar.urls"))
