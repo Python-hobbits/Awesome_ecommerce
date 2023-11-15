9c5ba0b1d0aea32d52160eb5fced4fda043b398d
Viacheslav Volodin <volodin28@gmail.com>
2023-11-15 17:49:59 +0200

    don't show querystring auth for static

--- src/config/storage_backends.py
+++ src/config/storage_backends.py
@@ -4,6 +4,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
+    querystring_auth = False
 
 
 class PublicMediaStorage(S3Boto3Storage):
